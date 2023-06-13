9e9ace4d779b574fe984a9b518753d8d345258aa
dpiolo <136399100+dpiolo@users.noreply.github.com>
2023-06-12 18:15:30 -0700

    Add files via upload

---
+++ Ball.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 17:46:21 2023
+
+@author: dpiolo
+"""
+
+v0 = int(input("Please input initial velocity: "))
+g = 32.8
+max_height = (v0**2)/(2*g)
+
+print(f"max height = {max_height}")
+
